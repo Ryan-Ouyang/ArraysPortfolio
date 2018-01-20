4fae0d3c7fc10f21fbd1aa32a1dea3e06901d067
ryan-ouyang <ryan.ouyang01@gmail.com>
2018-01-20 06:31:17 -0500

    All tickers added

--- js/button.js
+++ js/button.js
@@ -1,17 +1,18 @@
-$(function() {
-    $( "#mainButton" ).click(function() {
-      $( "#mainButton" ).addClass( "onclic", 250, validate);
+$(function () {
+    $("#mainButton").click(function () {
+        $("#mainButton").addClass("onclic", 250, validate);
     });
-  
+
     function validate() {
-      setTimeout(function() {
-        $( "#mainButton" ).removeClass( "onclic" );
-        $( "#mainButton" ).addClass( "validate", 450, callback );
-      }, 2250 );
+        setTimeout(function () {
+            $("#mainButton").removeClass("onclic");
+            $("#mainButton").addClass("validate", 450, callback);
+        }, 2250);
     }
-      function callback() {
-        setTimeout(function() {
-          $( "#mainButton" ).removeClass( "validate" );
-        }, 1250 );
-      }
-    });
+
+    function callback() {
+        setTimeout(function () {
+            $("#mainButton").removeClass("validate");
+        }, 1250);
+    }
+});
